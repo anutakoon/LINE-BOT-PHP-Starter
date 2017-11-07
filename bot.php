82a41a60410e23f21d9bf3cedc0c515a2abbbf33
ViGsAoS <anutakoon@hotmail.com>
2017-11-07 16:08:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,47 +1,11 @@
-<?php
-$access_token = 'uBiUUhIHsyf+CY+2JjrhO3h9yHO4pdwXCxQNoSPGfD9intmHZnOT02braDpML9FLVul5d4h4mJ+l087NGMwlp63l95j+3yhI3+gtWCScIjc84nazZsliAjUrW23CMRDL9DKcIvKjaH4Mm2u6r9Ri2gdB04t89/1O/w1cDnyilFU=';
+$bot = new \LINE\LINEBot(new CurlHTTPClient('uBiUUhIHsyf+CY+2JjrhO3h9yHO4pdwXCxQNoSPGfD9intmHZnOT02braDpML9FLVul5d4h4mJ+l087NGMwlp63l95j+3yhI3+gtWCScIjc84nazZsliAjUrW23CMRDL9DKcIvKjaH4Mm2u6r9Ri2gdB04t89/1O/w1cDnyilFU='), [
+    'channelSecret' => '63e60233af3a57edc4d6ee3a71b9ee16'
+]);
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
+$res = $bot->getProfile('user-id');
+if ($res->isSucceeded()) {
+    $profile = $res->getJSONDecodedBody();
+    $displayName = $profile['displayName'];
+    $statusMessage = $profile['statusMessage'];
+    $pictureUrl = $profile['pictureUrl'];
 }
-echo "OK";
